6b634e172301b4bb7153d21ad6b83775be47579d
Adam Zamojski <ajzamojski@gmail.com>
2017-09-22 16:40:24 -0400

    added js file and new visibility settings with thumbnails

---
+++ assets/js/portfolio.js
@@ -0,0 +1,13 @@
+
+
+$('.div-image').mouseover(function() {
+  $(this).find('.text-in-image').css({
+  	"visibility" : "visible"
+  });
+  $(this).find("img").css({"opacity" : ".3"});
+});
+
+$('.div-image').mouseout(function() {
+  $(this).find('.text-in-image').css("visibility","hidden");
+  $(this).find("img").css({"opacity" : "1"});
+});
