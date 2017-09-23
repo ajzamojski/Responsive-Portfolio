d1ff11ac2f43228cba4fbd3ead23787d56ef38ee
Adam Zamojski <ajzamojski@gmail.com>
2017-09-23 16:38:30 -0400

    adding load functionality

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,16 +1,17 @@
 
 // mouseover effects for portfolio tab
-$('.div-image').mouseover(function() {
-  $(this).find('.text-in-image').css({
-  	"visibility" : "visible"
-  });
-  $(this).find("img").css({"opacity" : ".3"});
-});
+$( window ).on( "load", function() {
+	$('.div-image').mouseover(function() {
+	  $(this).find('.text-in-image').css({
+	  	"visibility" : "visible"
+	  });
+	  $(this).find("img").css({"opacity" : ".3"});
+	});
 
-$('.div-image').mouseout(function() {
-  $(this).find('.text-in-image').css("visibility","hidden");
-  $(this).find("img").css({"opacity" : "1"});
+	$('.div-image').mouseout(function() {
+	  $(this).find('.text-in-image').css("visibility","hidden");
+	  $(this).find("img").css({"opacity" : "1"});
+	});
 });
-
 
 
